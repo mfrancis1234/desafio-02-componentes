fea5db3e34041500d20881b32babae882f9e0f01
Francisco Esperança <mfrancis_1@hotmail.com>
2022-03-20 00:05:18 -0500

    first commit

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,46 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  movies: IMovies[];
+  selectedGenre: {
+    title: string;
+  };
+}
+
+interface IMovies {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+export function Content(props: ContentProps) {
   // Complete aqui
+
+  return (
+    <>
+      <header>
+        <span className='category'>
+          Categoria:<span> {props.selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className='movies-list'>
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: IGenres[];
+  setSelectedGenreId: (id: number) => void;
+  selectedGenreId: number;
+}
+
+interface IGenres {
+  id: number;
+  title: string;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+  function handleClickButton(id: number) {
+    props.setSelectedGenreId(id);
+  }
+
+  return (
+    <>
+      <span>
+        Watch<p>Me</p>
+      </span>
+      <div className='buttons-container'>
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
